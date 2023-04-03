a14147125984856bffcdfca7f24cbe756c0391d8
QwEekYhyo <qweekyhyo.yt83@gmail.com>
2023-04-03 20:09:53 +0200

    added at() and [] operator

--- matrix.hpp
+++ matrix.hpp
@@ -2,6 +2,7 @@
 #define REEL_BEAST_MATRIX
 
 #include <array>
+#include <stdexcept>
 
 template <typename T, std::size_t rows, std::size_t columns = rows>
 class matrix {
@@ -15,8 +16,14 @@ class matrix {
         inline matrix();
         inline matrix(std::array<std::array<T, columns>, rows>);
 
+        inline T& operator[](index i);
+
+        inline T& at(index i);
+        inline const T& at(index i) const;
+
         std::array<std::array<T, columns>, rows> m_buffer{0}; //this will be private
     private:
+        void ensure_range(index i) const;
 };
 
 template <typename T, std::size_t rows, std::size_t columns>
@@ -25,5 +32,28 @@ matrix<T, rows, columns>::matrix() = default;
 template <typename T, std::size_t rows, std::size_t columns>
 matrix<T, rows, columns>::matrix(std::array<std::array<T, columns>, rows> arr)
     : m_buffer{std::move(arr)} {}
+
+template <typename T, std::size_t rows, std::size_t columns>
+T& matrix<T, rows, columns>::operator[](matrix::index i) {
+    return m_buffer[i.y][i.x];
+}
+
+template <typename T, std::size_t rows, std::size_t columns>
+void matrix<T, rows, columns>::ensure_range(matrix::index i) const {
+    if (i.x >= columns || i.y >= rows)
+        throw std::out_of_range("matrix index out of range");
+}
+
+template <typename T, std::size_t rows, std::size_t columns>
+T& matrix<T, rows, columns>::at(matrix::index i) {
+    ensure_range(i);
+    return m_buffer.at(i.y).at(i.x);
+}
+
+template <typename T, std::size_t rows, std::size_t columns>
+const T& matrix<T, rows, columns>::at(matrix::index i) const {
+    ensure_range(i);
+    return m_buffer.at(i.y).at(i.x);
+}
 
 #endif 
--- test.cpp
+++ test.cpp
@@ -4,13 +4,15 @@
 using line = std::array<int, 3>;
 
 int main() {
-    matrix<int, 3> m = std::array<line, 3>{line{1, 2, 3}, line{4, 5, 6}, line{7, 8, 9}};
+    matrix<int, 3> m = {{1, 2, 3, 4, 5, 6, 7, 8, 9}};
     for (auto arr : m.m_buffer) {
         for (int n : arr) {
             std::cout << n << " ";
         }
         std::cout << '\n';
     }
+    std::cout << m.at({0, 0}) << std::endl;
+    std::cout << m.at({2, 3}) << std::endl; // throws error nice :)
 
     return 0;
 }
